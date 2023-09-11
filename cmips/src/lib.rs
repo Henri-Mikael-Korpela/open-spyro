576c6685393495d9bf950c9bf5c4c4bd44758c02
Henri Korpela <henri.korpela@vincit.fi>
2023-09-11 06:56:10 +0300

    Add support for false boolean literal token

--- cmips/src/lib.rs
+++ cmips/src/lib.rs
@@ -4,6 +4,7 @@ pub enum Token<'a> {
     KeywordIf,
     KeywordProc,
     KeywordPub,
+    LiteralBoolFalse,
     LiteralBoolTrue,
     OperatorBraceClose,
     OperatorBraceOpen,
@@ -34,6 +35,38 @@ pub fn tokenize(code: &str) -> Vec<Token> {
                 '{' => tokens.push(Token::OperatorBraceOpen),
                 ')' => tokens.push(Token::OperatorParenthesisClose),
                 '(' => tokens.push(Token::OperatorParenthesisOpen),
+                'f' => {
+                    // If the following characters result in "false"
+                    if (i + 4) < code_chars_count
+                        && char_at!(i + 1) == 'a'
+                        && char_at!(i + 2) == 'l'
+                        && char_at!(i + 3) == 's'
+                        && char_at!(i + 4) == 'e'
+                    {
+                        tokens.push(Token::LiteralBoolFalse);
+                        i += 5;
+                        continue;
+                    } else {
+                        if c.is_alphabetic() {
+                            let identifier_begin = i;
+                            i += 1;
+                            'identifier_loop: while let Some(c) = code.chars().nth(i) {
+                                if c.is_alphanumeric() || c == '_' {
+                                    i += 1;
+                                    // If there are no characters left after identifier, push the identifier
+                                    if i >= code_chars_count {
+                                        tokens.push(Token::Identifier(&code[identifier_begin..i]));
+                                        break 'identifier_loop;
+                                    }
+                                } else {
+                                    tokens.push(Token::Identifier(&code[identifier_begin..i]));
+                                    i -= 1;
+                                    break 'identifier_loop;
+                                }
+                            }
+                        }
+                    }
+                }
                 'i' => {
                     // If the following characters result in "if"
                     if (i + 1) < code_chars_count && char_at!(i + 1) == 'f' {
@@ -156,60 +189,87 @@ pub fn tokenize(code: &str) -> Vec<Token> {
     tokens
 }
 
-#[test]
-fn should_tokenize_identifier() {
-    // Identifier with only alphabetic characters
-    let tokens = tokenize("main");
-    assert_eq!(tokens.len(), 1);
-    assert_eq!(tokens[0], Token::Identifier("main"));
+#[cfg(test)]
+mod tokenization_test {
+    use super::*;
 
-    // Identifier with underscore
-    let tokens = tokenize("main_loop");
-    assert_eq!(tokens.len(), 1);
-    assert_eq!(tokens[0], Token::Identifier("main_loop"));
+    #[test]
+    fn should_tokenize_boolean_literals() {
+        // Boolean literal false
+        let tokens = tokenize("false");
+        assert_eq!(tokens.len(), 1);
+        assert_eq!(tokens[0], Token::LiteralBoolFalse);
 
-    // Identifier with underscore and number
-    let tokens = tokenize("main_loop_1");
-    assert_eq!(tokens.len(), 1);
-    assert_eq!(tokens[0], Token::Identifier("main_loop_1"));
-}
-#[test]
-fn should_tokenize_public_procedure_definition() {
-    // Procedure definition without parameters.
-    let tokens = tokenize("pub proc main(){}");
-    assert_eq!(tokens.len(), 7);
-    assert_eq!(tokens[0], Token::KeywordPub);
-    assert_eq!(tokens[1], Token::KeywordProc);
-    assert_eq!(tokens[2], Token::Identifier("main"));
-    assert_eq!(tokens[3], Token::OperatorParenthesisOpen);
-    assert_eq!(tokens[4], Token::OperatorParenthesisClose);
-    assert_eq!(tokens[5], Token::OperatorBraceOpen);
-    assert_eq!(tokens[6], Token::OperatorBraceClose);
+        // Boolean literal true
+        let tokens = tokenize("true");
+        assert_eq!(tokens.len(), 1);
+        assert_eq!(tokens[0], Token::LiteralBoolTrue);
 
-    // Procedure definition with parameters with a procedure name that begins with 'p'
-    // that should not mess up with the keywords starting with 'p'.
-    let tokens = tokenize("pub proc poll(){}");
-    assert_eq!(tokens.len(), 7);
-    assert_eq!(tokens[0], Token::KeywordPub);
-    assert_eq!(tokens[1], Token::KeywordProc);
-    assert_eq!(tokens[2], Token::Identifier("poll"));
-    assert_eq!(tokens[3], Token::OperatorParenthesisOpen);
-    assert_eq!(tokens[4], Token::OperatorParenthesisClose);
-    assert_eq!(tokens[5], Token::OperatorBraceOpen);
-    assert_eq!(tokens[6], Token::OperatorBraceClose);
+        let tokens = tokenize("proc main(){false}");
+        assert_eq!(tokens.len(), 7);
+        assert_eq!(tokens[0], Token::KeywordProc);
+        assert_eq!(tokens[1], Token::Identifier("main"));
+        assert_eq!(tokens[2], Token::OperatorParenthesisOpen);
+        assert_eq!(tokens[3], Token::OperatorParenthesisClose);
+        assert_eq!(tokens[4], Token::OperatorBraceOpen);
+        assert_eq!(tokens[5], Token::LiteralBoolFalse);
+        assert_eq!(tokens[6], Token::OperatorBraceClose);
+    }
+    #[test]
+    fn should_tokenize_identifier() {
+        // Identifier with only alphabetic characters
+        let tokens = tokenize("main");
+        assert_eq!(tokens.len(), 1);
+        assert_eq!(tokens[0], Token::Identifier("main"));
 
-    // Procedure definition with body containing an if statement.
-    let tokens = tokenize("pub proc main(){if true{}}");
-    assert_eq!(tokens.len(), 11);
-    assert_eq!(tokens[0], Token::KeywordPub);
-    assert_eq!(tokens[1], Token::KeywordProc);
-    assert_eq!(tokens[2], Token::Identifier("main"));
-    assert_eq!(tokens[3], Token::OperatorParenthesisOpen);
-    assert_eq!(tokens[4], Token::OperatorParenthesisClose);
-    assert_eq!(tokens[5], Token::OperatorBraceOpen);
-    assert_eq!(tokens[6], Token::KeywordIf);
-    assert_eq!(tokens[7], Token::LiteralBoolTrue);
-    assert_eq!(tokens[8], Token::OperatorBraceOpen);
-    assert_eq!(tokens[9], Token::OperatorBraceClose);
-    assert_eq!(tokens[10], Token::OperatorBraceClose);
+        // Identifier with underscore
+        let tokens = tokenize("main_loop");
+        assert_eq!(tokens.len(), 1);
+        assert_eq!(tokens[0], Token::Identifier("main_loop"));
+
+        // Identifier with underscore and number
+        let tokens = tokenize("main_loop_1");
+        assert_eq!(tokens.len(), 1);
+        assert_eq!(tokens[0], Token::Identifier("main_loop_1"));
+    }
+    #[test]
+    fn should_tokenize_public_procedure_definition() {
+        // Procedure definition without parameters.
+        let tokens = tokenize("pub proc main(){}");
+        assert_eq!(tokens.len(), 7);
+        assert_eq!(tokens[0], Token::KeywordPub);
+        assert_eq!(tokens[1], Token::KeywordProc);
+        assert_eq!(tokens[2], Token::Identifier("main"));
+        assert_eq!(tokens[3], Token::OperatorParenthesisOpen);
+        assert_eq!(tokens[4], Token::OperatorParenthesisClose);
+        assert_eq!(tokens[5], Token::OperatorBraceOpen);
+        assert_eq!(tokens[6], Token::OperatorBraceClose);
+
+        // Procedure definition with parameters with a procedure name that begins with 'p'
+        // that should not mess up with the keywords starting with 'p'.
+        let tokens = tokenize("pub proc poll(){}");
+        assert_eq!(tokens.len(), 7);
+        assert_eq!(tokens[0], Token::KeywordPub);
+        assert_eq!(tokens[1], Token::KeywordProc);
+        assert_eq!(tokens[2], Token::Identifier("poll"));
+        assert_eq!(tokens[3], Token::OperatorParenthesisOpen);
+        assert_eq!(tokens[4], Token::OperatorParenthesisClose);
+        assert_eq!(tokens[5], Token::OperatorBraceOpen);
+        assert_eq!(tokens[6], Token::OperatorBraceClose);
+
+        // Procedure definition with body containing an if statement.
+        let tokens = tokenize("pub proc main(){if true{}}");
+        assert_eq!(tokens.len(), 11);
+        assert_eq!(tokens[0], Token::KeywordPub);
+        assert_eq!(tokens[1], Token::KeywordProc);
+        assert_eq!(tokens[2], Token::Identifier("main"));
+        assert_eq!(tokens[3], Token::OperatorParenthesisOpen);
+        assert_eq!(tokens[4], Token::OperatorParenthesisClose);
+        assert_eq!(tokens[5], Token::OperatorBraceOpen);
+        assert_eq!(tokens[6], Token::KeywordIf);
+        assert_eq!(tokens[7], Token::LiteralBoolTrue);
+        assert_eq!(tokens[8], Token::OperatorBraceOpen);
+        assert_eq!(tokens[9], Token::OperatorBraceClose);
+        assert_eq!(tokens[10], Token::OperatorBraceClose);
+    }
 }
