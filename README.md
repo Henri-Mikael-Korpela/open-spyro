3dcc4b794bc7de105fd3ba16241adfbff3c37c8d
Henri Korpela <henri.korpela@vincit.fi>
2023-10-19 08:28:00 +0300

    Update documentation, fix spelling mistakes and make info more clear

--- README.md
+++ README.md
@@ -4,22 +4,23 @@
 
 ## Introduction
 
-Welcome to the Spyro the Dragon PC port project called OpenSpyro! OpenSpyro is an open-source initiative driven by a passionate programmer and fan of the original Spyro the Dragon trilogy on Playstation. The project is focused on reverse-engineering and adapting the game to run natively on Linux and Windows. I aim to provide an authentic experience that stays true to the original while leveraging the capabilities of modern hardware.
+Welcome to the Spyro the Dragon Linux and Windows port project called OpenSpyro! OpenSpyro is an open-source initiative driven by a passionate programmer and fan of the original Spyro the Dragon trilogy on Playstation. The project is focused on reverse-engineering and adapting the game to run natively on Linux and Windows (I don't have a Mac, sorry). I aim to provide an authentic experience that stays true to the original while leveraging the capabilities of modern hardware.
 
-This project will not contain any game data in its original form, you must have an original ROM of the game available in order to play this port.
+This project will not contain any game data in its original form. You must have an original ROM of the game available in order to play this port.
 
 ## Goals
 
-* No emulation required to run the game on Linux and Windows
-* Maintaining the original experience while leveraging the capabilities of modern hardware
+* Make the game run natively, no emulation needed to run the game on Linux or Windows.
+* Maintaining the original experience as is while leveraging the capabilities of modern hardware. The focus is on rewriting only the necessary logic to get the game running on Linux and Windows, although some quality of life improvements may be made.
+* Support for mods in some way.
 
 ## Current state
 
-The project is currently in its early stages. Port is currently unplayable. Some of the MIPS assembly code has been disassembled with comments and labels added. Disassembly is into a completely custom assembly, this repository even has its own assembler and disassembler! This custom assembler and disassembler ought to have more documentation, but it is still in the works.
+The project is currently in its early stages. Port is currently unplayable. Some of the MIPS assembly code has been disassembled with comments and labels added. Disassembly is in a completely custom assembly, the repository even has its own assembler and disassembler! This custom assembler and disassembler ought to have more documentation, but it is still in the works.
 
 The project is currently focused on reverse-engineering the MIPS assembly and understanding how the data is layed out on the ROM. Some tools have been built along the way, like a working ROM file replacer, which is used for saving Playstation executable files back into the ROM file. Some work has been started on reading WAD files, but it is in its early stages.
 
-ROM currently supported is an NTSC version. I have not worked on any other versions yet.
+ROM currently worked on is an NTSC version. I have not worked on any other versions yet.
 
 ## Commands
 
--- src/main.rs
+++ src/main.rs
@@ -96,19 +96,20 @@ fn generate_doc(_args: &[String]) -> Result<(), Box<dyn std::error::Error>> {
         String::from("![Spyro the Dragon screenshot from Town Square](http://henrijahanna.fi/projects/open_spyro/spyro_town_square.bmp)"),
         
         title(2, "Introduction"),
-        String::from("Welcome to the Spyro the Dragon PC port project called OpenSpyro! OpenSpyro is an open-source initiative driven by a passionate programmer and fan of the original Spyro the Dragon trilogy on Playstation. The project is focused on reverse-engineering and adapting the game to run natively on Linux and Windows. I aim to provide an authentic experience that stays true to the original while leveraging the capabilities of modern hardware."),
-        String::from("This project will not contain any game data in its original form, you must have an original ROM of the game available in order to play this port."),
+        String::from("Welcome to the Spyro the Dragon Linux and Windows port project called OpenSpyro! OpenSpyro is an open-source initiative driven by a passionate programmer and fan of the original Spyro the Dragon trilogy on Playstation. The project is focused on reverse-engineering and adapting the game to run natively on Linux and Windows (I don't have a Mac, sorry). I aim to provide an authentic experience that stays true to the original while leveraging the capabilities of modern hardware."),
+        String::from("This project will not contain any game data in its original form. You must have an original ROM of the game available in order to play this port."),
 
         title(2, "Goals"),
         list_bulleted(&[
-            "No emulation required to run the game on Linux and Windows",
-            "Maintaining the original experience while leveraging the capabilities of modern hardware",
+            "Make the game run natively, no emulation needed to run the game on Linux or Windows.",
+            "Maintaining the original experience as is while leveraging the capabilities of modern hardware. The focus is on rewriting only the necessary logic to get the game running on Linux and Windows, although some quality of life improvements may be made.",
+            "Support for mods in some way."
         ]),
 
         title(2, "Current state"),
-        String::from("The project is currently in its early stages. Port is currently unplayable. Some of the MIPS assembly code has been disassembled with comments and labels added. Disassembly is into a completely custom assembly, this repository even has its own assembler and disassembler! This custom assembler and disassembler ought to have more documentation, but it is still in the works."),
+        String::from("The project is currently in its early stages. Port is currently unplayable. Some of the MIPS assembly code has been disassembled with comments and labels added. Disassembly is in a completely custom assembly, the repository even has its own assembler and disassembler! This custom assembler and disassembler ought to have more documentation, but it is still in the works."),
         String::from("The project is currently focused on reverse-engineering the MIPS assembly and understanding how the data is layed out on the ROM. Some tools have been built along the way, like a working ROM file replacer, which is used for saving Playstation executable files back into the ROM file. Some work has been started on reading WAD files, but it is in its early stages."),
-        String::from("ROM currently supported is an NTSC version. I have not worked on any other versions yet."),
+        String::from("ROM currently worked on is an NTSC version. I have not worked on any other versions yet."),
 
         title(2, "Commands"),
         String::from("Here's a list CLI commands currently supported:"),
